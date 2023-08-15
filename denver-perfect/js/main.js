81060225db182bd16684b6c1d9c6f484a9abe3ca
Rafiur Rahman <rrfoundary@gmail.com>
2023-08-15 19:58:23 +0600

    added sections before footer

--- denver-perfect/js/main.js
+++
@@ -1,6 +0,0 @@
-const dropdown = document.querySelector(".dropdown");
-
-dropdown.addEventListener("mouseleave", () => {
-  const dropdownMenu = dropdown.querySelector(".dropdown-menu");
-  dropdownMenu.style.display = "none";
-});
