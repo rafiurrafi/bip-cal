691f4b69acdf5f1ba89a4af9cfda40c6074faf2a
Rafiur Rahman <rrfoundary@gmail.com>
2023-08-19 01:31:35 +0600

    fixed small problems

--- denver-perfect/js/main.js
+++ denver-perfect/js/main.js
@@ -1,34 +1,34 @@
-const numberContent = document.querySelector(".numbers-content");
-const sectionNumber = document.querySelector(".section-numbers");
-const sectionColor = document.querySelector(".section-color");
+// const numberContent = document.querySelector(".numbers-content");
+// const sectionNumber = document.querySelector(".section-numbers");
+// const sectionColor = document.querySelector(".section-color");
 
-const sectionSecurity = document.querySelector(".section-security");
-const blueBg = document.querySelector(".bg-blue-lightest");
-const sectionSecurityImg = document.querySelector(".section-security-img");
+// const sectionSecurity = document.querySelector(".section-security");
+// const blueBg = document.querySelector(".bg-blue-lightest");
+// const sectionSecurityImg = document.querySelector(".section-security-img");
 
-const heroContent = document.querySelector(".hero-text");
-const heroContentContainer = document.querySelector(".section-hero .container");
-const sectionHero = document.querySelector(".section-hero");
+// const heroContent = document.querySelector(".hero-text");
+// const heroContentContainer = document.querySelector(".section-hero .container");
+// const sectionHero = document.querySelector(".section-hero");
 
 //change size of section number grid
-let numberContentHeight = numberContent.clientHeight;
+// let numberContentHeight = numberContent.clientHeight;
 
-if (window.innerWidth > 768) {
-  sectionNumber.style.height = numberContentHeight + "px";
-  sectionColor.style.height = numberContentHeight * 0.5 + "px";
-} else {
-  sectionNumber.style.height = numberContentHeight * 0.9 + "px";
-  sectionColor.style.height = numberContentHeight * 0.4 + "px";
-}
+// if (window.innerWidth > 768) {
+//   sectionNumber.style.height = numberContentHeight + "px";
+//   sectionColor.style.height = numberContentHeight * 0.5 + "px";
+// } else {
+//   sectionNumber.style.height = numberContentHeight * 0.9 + "px";
+//   sectionColor.style.height = numberContentHeight * 0.4 + "px";
+// }
 
 //change height of security section
-let blueBgHeight = blueBg.clientHeight;
+// let blueBgHeight = blueBg.clientHeight;
 // let blugBgWidth = blueBg.clientWidth;
-sectionSecurity.style.height = blueBgHeight * 1.07 + "px";
-sectionSecurityImg.style.height = blueBgHeight * 1.07 + "px";
+// sectionSecurity.style.height = blueBgHeight * 1.2 + "px";
+// sectionSecurityImg.style.height = blueBgHeight * 1.2 + "px";
 // sectionSecurityImg.style.width = blugBgWidth + "px";
 
 //section hero
-let heroTextHeight = heroContent.clientHeight;
-heroContentContainer.style.height = heroTextHeight + "px";
-sectionHero.style.height = heroTextHeight + "px";
+// let heroTextHeight = heroContent.clientHeight;
+// heroContentContainer.style.height = heroTextHeight + "px";
+// sectionHero.style.height = heroTextHeight + "px";
