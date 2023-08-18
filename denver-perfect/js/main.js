c21a035a864822400402ddc7f2cd970e64ea1f52
Rafiur Rahman <rrfoundary@gmail.com>
2023-08-18 11:11:46 +0600

    fix responsive prblm in section security

--- denver-perfect/js/main.js
+++ denver-perfect/js/main.js
@@ -2,11 +2,25 @@ const numberContent = document.querySelector(".numbers-content");
 const sectionNumber = document.querySelector(".section-numbers");
 const sectionColor = document.querySelector(".section-color");
 
-let numberContentHeight = numberContent.clientHeight;
-sectionNumber.style.height = numberContentHeight * 0.8 + "px";
-sectionColor.style.height = numberContentHeight * 0.4 + "px";
+const sectionSecurity = document.querySelector(".section-security");
+const blueBg = document.querySelector(".bg-blue-lightest");
+const sectionSecurityImg = document.querySelector(".section-security-img");
 
-console.log(window.innerWidth);
+let numberContentHeight = numberContent.clientHeight;
+
+if (window.innerWidth > 768) {
+  sectionNumber.style.height = numberContentHeight * 0.9 + "px";
+  sectionColor.style.height = numberContentHeight * 0.5 + "px";
+} else {
+  sectionNumber.style.height = numberContentHeight * 0.8 + "px";
+  sectionColor.style.height = numberContentHeight * 0.4 + "px";
+}
+
+let blueBgHeight = blueBg.clientHeight;
+sectionSecurity.style.height = blueBgHeight + "px";
+sectionSecurityImg.style.height = blueBgHeight + "px";
+
+console.log(blueBgHeight);
 
 //init wow js
 //
