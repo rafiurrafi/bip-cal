d3681eb2fb3f250c53996f51b7b74db12aded7ed
Rafiur Rahman <rrfoundary@gmail.com>
2023-08-22 15:22:01 +0600

    fixed hero text image problem

--- denver-perfect/js/main.js
+++ denver-perfect/js/main.js
@@ -1,14 +1,13 @@
 const heroContent = document.querySelector(".hero-text");
 const heroImg = document.querySelector(".section-hero-bg");
 const sectionContact = document.querySelector(".section-contact");
-// const tabletSize = 991;
+
 function resizeListener() {
   const heighOfContent =
     heroContent.getBoundingClientRect().bottom -
     heroImg.getBoundingClientRect().bottom;
-  console.log(heighOfContent);
   sectionContact.style.marginTop =
-    heighOfContent > 0 ? heighOfContent + "px" : 0 + "px";
+    heighOfContent > 0 ? heighOfContent + 24 + "px" : 24 + "px";
 }
 resizeListener();
 window.addEventListener("resize", resizeListener);
